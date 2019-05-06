456a758528ee6773761e07c6b0805996e1bff273
nizhongxing <49489510+nizhongxing@users.noreply.github.com>
2019-05-06 09:32:29 +0800

    777

--- nzx/src/main/java/nzx/Test01.java
+++ nzx/src/main/java/nzx/Test01.java
@@ -3,6 +3,7 @@ package nzx;
 public class Test01 {
 
 	public static void main(String[] args) {
-		System.out.println("jiayou");
+		System.out.println("加油！！！");
+		System.out.println("6666");
 	}
 }
